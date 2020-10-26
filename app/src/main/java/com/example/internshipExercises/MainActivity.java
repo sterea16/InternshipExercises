fb14f6db8cf7e4a6d275da7aca54330ad431f7ac
sterea16 <54959323+sterea16@users.noreply.github.com>
2020-10-26 17:52:33 +0200

    Day 5 exercise 1 Introducing map view and playing with the marker's onClickListener.

--- app/src/main/java/com/example/internshipExercises/MainActivity.java
+++ app/src/main/java/com/example/internshipExercises/MainActivity.java
@@ -2,38 +2,42 @@ package com.example.internshipExercises;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.Menu;
 
-import com.example.internshipExercises.util.DataBindExecutor;
-import com.example.internshipExercises.util.DownloaderUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.image_view);
-        /*downloadImageUsingThread();*/
-        downloadImageUsingExecutor();
+        MapView mapView = findViewById(R.id.maps);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        mapView.onResume();
     }
 
-    private void downloadImageUsingExecutor() {
-        Runnable runnable = () -> {
-            Bitmap bitmap = DownloaderUtil.INSTANCE.downloadImage();
-            runOnUiThread(() -> imageView.setImageBitmap(bitmap));
-        };
-        DataBindExecutor downloadExecutor = new DataBindExecutor();
-        downloadExecutor.execute(runnable);
-    }
 
-    private void downloadImageUsingThread(){
-        new Thread(() -> {
-            final Bitmap bitmap = DownloaderUtil.INSTANCE.downloadImage();
-            runOnUiThread(() -> imageView.setImageBitmap(bitmap));
-        }).start();
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng coordinates = new LatLng(46.768150,23.570607);
+        googleMap.addMarker(new MarkerOptions().position(coordinates));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 16f));
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.setTitle(getResources().getString(R.string.marker_title));
+                return false;
+            }
+        });
     }
 }
